6e2def27f6bd38fcc5d4ffca020f25b05b6c9a78
SaraChiriboga <sara.chiriboga.valle@udla.edu.ec>
2024-10-16 10:19:47 -0500

    S3 completa

--- SEMANA 3/Deporte/src/Jugadores.java
+++ SEMANA 3/Deporte/src/Jugadores.java
@@ -1,6 +1,63 @@
 public class Jugadores {
     //atributos
+    String nombre;
+    String apellido;
+    Integer numCamiseta;
+    String posicion;
+
     //metodos setter y getter
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getNumCamiseta() {
+        return numCamiseta;
+    }
+
+    public void setNumCamiseta(Integer numCamiseta) {
+        this.numCamiseta = numCamiseta;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+    //constructor para evitar el getter y setter
+    public Jugadores(String nombre, String apellido, Integer numCamiseta, String posicion){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numCamiseta = numCamiseta;
+        this.posicion = posicion;
+    }
+
     //metodos
     //imprimir alineacion
+    public void imprimirAlineacion343(String arquero, String dDerecho, String dCentral, String dIzquierdo, String vDerecho, String vCentral1, String vCentral2, String vIzquierdo, String delantero1, String delantero2, String delantero3){
+        System.out.println("==================== ECUADOR 3-4-3 ====================");
+        System.out.println("                      " + arquero);
+        System.out.println("\n");
+        System.out.println("     " + dDerecho + "          " + dCentral + "          " + dIzquierdo);
+        System.out.println("\n");
+        System.out.println("  " + vDerecho + "   " + vCentral1 + "   " + vCentral2 + "   " + vIzquierdo);
+        System.out.println("\n");
+        System.out.println("     " + delantero1 + "          " + delantero2 + "          " + delantero3);
+    }
+
+    //imprimir info de cada jugador
 }
--- SEMANA 3/Deporte/src/Main.java
+++ SEMANA 3/Deporte/src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//FUTBOL 11
+//ECUADOR 3-4-3
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //instanciar: jugadores
+        Jugadores galindez = new Jugadores("Hernan", "Galindez", 1, "Arquero");
+        Jugadores torres = new Jugadores("Felix", "Torres", 2, "Defensa Derecho");
+        Jugadores pacho = new Jugadores("William", "Pacho", 6, " Defensa Central");
+        Jugadores pierito = new Jugadores("Piero", "Hincapie", 3, "Defensa Izquierdo");
+        Jugadores preciado = new Jugadores("Angelo", "Preciado", 17, "Volante Derecho");
+        Jugadores franco = new Jugadores("Alan", "Franco", 21, "Volante Central");
+        Jugadores moi = new Jugadores("Moises", "Caicedo", 23, "Volante Central"); //capitan
+        Jugadores pervis = new Jugadores("Pervis", "Estupiñan", 7, "Volante Izquierdo");
+        Jugadores plata = new Jugadores("Gonzalo", "Plata", 19, "Delantero");
+        Jugadores valencia = new Jugadores("Enner", "Valencia", 13, "Delantero");
+        Jugadores sarmiento = new Jugadores("Jeremy", "Sarmiento", 16, "Delantero");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //alineacion
+        //usando el objeto capitan para hacer la alineacion
+        moi.imprimirAlineacion343(galindez.apellido + "(" + galindez.numCamiseta +")", torres.apellido + "(" + torres.numCamiseta +")",
+                pacho.apellido + "(" + pacho.numCamiseta +")", pierito.apellido + "(" + pierito.numCamiseta +")",
+                preciado.apellido + "(" + preciado.numCamiseta +")", franco.apellido + "(" + franco.numCamiseta +")",
+                moi.apellido + "(" + moi.numCamiseta +")", pervis.apellido + "(" + pervis.numCamiseta +")",
+                plata.apellido + "(" + plata.numCamiseta +")", valencia.apellido + "(" + valencia.numCamiseta +")",
+                sarmiento.apellido + "(" + sarmiento.numCamiseta +")");
     }
 }
