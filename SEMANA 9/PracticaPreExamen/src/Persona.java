d63fd9d308b25a599e4ee8a7c8b4c2802e7613fe
SaraChiriboga <sara.chiriboga.valle@udla.edu.ec>
2024-12-01 21:46:05 -0500

    P2v2

---
+++ SEMANA 9/PracticaPreExamen/src/Persona.java
@@ -0,0 +1,39 @@
+public class Persona {
+    private String nombrecliente;
+    private String email;
+    private String direccion;
+    private String telefono;
+    private String ID;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNombrecliente() {
+        return nombrecliente;
+    }
+
+    public void setNombrecliente(String nombrecliente) {
+        this.nombrecliente = nombrecliente;
+    }
+}
